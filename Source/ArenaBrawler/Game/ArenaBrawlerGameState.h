5e80b305ba9cd5bce21a5f5708d47b759f8235b2
Carlos <carlos.piccolini@hotmail.com>
2025-05-03 11:47:55 -0300

    Base setup classes added

---
+++ Source/ArenaBrawler/Game/ArenaBrawlerGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "ArenaBrawlerGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABRAWLER_API AArenaBrawlerGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ArenaBrawler/Game/ArenaBrawlerPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ArenaBrawlerPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABRAWLER_API AArenaBrawlerPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
